fb6c8e371e2480af4b75995d5b91474c9560a5f3
PawelOchal <opawel262@gmail.com>
2023-11-25 15:34:59 +0100

    added and cofigured swagger

--- bitchain/bitchain/settings.py
+++ bitchain/bitchain/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party apps
+    'rest_framework',
+    'drf_spectacular',
+    
     # Local apps
     'core',
 ]
@@ -129,3 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
