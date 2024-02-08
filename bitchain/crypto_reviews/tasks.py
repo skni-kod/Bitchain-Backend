a591bfd75ddf14e9be436db2177e40f1f1db0021
PawelOchal <opawel262@gmail.com>
2024-02-08 13:04:44 +0100

    added exception for tasks

--- bitchain/crypto_reviews/tasks.py
+++ bitchain/crypto_reviews/tasks.py
@@ -1,12 +1,18 @@
 from celery import shared_task
 from django.utils import timezone
 from .models import CryptoReview
+from django.db.utils import ProgrammingError as django_db_ProgrammingError
 
 @shared_task
 def reset_counts():
-    # Reset counts for CryptoReview
-    CryptoReview.objects.all().update(good=0, bad=0, last_reset_date=timezone.now().date())
-    print("CryptoReview counts reset")
+    """
+     Resets 'good' and 'bad' counts for all CryptoReview objects. This task is executed every day at 00:00.
+    """
+    try:
+        CryptoReview.objects.all().update(good=0, bad=0, last_reset_date=timezone.now().date())
+        print("CryptoReview counts reset")
+    except django_db_ProgrammingError:
+        print("Database not ready yet. Skipping reset_counts task.")
 
 @shared_task
 def initialize_on_startup_check_update_crypto_review():
@@ -19,10 +25,13 @@ def initialize_on_startup_check_update_crypto_review():
     If no update is needed, it prints a message indicating that no update is required.
 
     """
-    last_update_date = CryptoReview.objects.values_list('last_reset_date', flat=True).first()
-    if last_update_date != timezone.now().date():
-        # Perform the update if needed
-        CryptoReview.objects.all().update(good=0, bad=0, last_reset_date=timezone.now().date())
-        print("Initialization task on Docker startup - Data updated\n")
-    else:
-        print("Initialization task on Docker startup - No update needed\n")
+    try: 
+        last_update_date = CryptoReview.objects.values_list('last_reset_date', flat=True).first()
+        if last_update_date != timezone.now().date():
+            # Perform the update if needed
+            CryptoReview.objects.all().update(good=0, bad=0, last_reset_date=timezone.now().date())
+            print("Initialization task on Docker startup - Data updated\n")
+        else:
+            print("Initialization task on Docker startup - No update needed\n")
+    except django_db_ProgrammingError:
+        print("Database not ready yet. Skipping initialization task.")
